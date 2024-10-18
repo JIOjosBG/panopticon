0d7e3a90071be7cbba2ed507dcf93376d1c86b72
Yosif Hamed <josif.hameed@gmail.com>
2024-10-18 17:56:41 +0300

    add route for rss feeds

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { fetchAllRssFeeds } from './utils.js'
+const app = express();
+const port = 3000;
+
+app.get('/', async (req, res) => {
+    let feeds = await fetchAllRssFeeds()
+    res.json(feeds);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ server/utils.js
@@ -0,0 +1,18 @@
+import Parser from 'rss-parser';
+const parser = new Parser();
+const RSS_FEEDS = [
+    // 'https://feed.podbean.com/web3onfire/feed.xml',
+    'https://decrypt.co/feed',
+    // 'https://www.coindesk.com/arc/outboundfeeds/rss/?_gl=1*byjpx3*_up*MQ..*_ga*MTg1ODczMTQ3MC4xNzI5MjQ2MTc1*_ga_VM3STRYVN8*MTcyOTI0NjE3NC4xLjAuMTcyOTI0NjE3NC4wLjAuODQzMTk5OTI3',
+    // 'https://cointelegraph.com/rss',
+    'https://bitcoinmagazine.com/.rss/full/',
+    // 'https://cryptoslate.com/feed/',
+    'https://www.vitoshacademy.com/feed/'
+];
+
+
+export async function fetchAllRssFeeds() {
+    return Promise.all(RSS_FEEDS.map(feed=>{
+        return parser.parseURL(feed);
+    }))
+}
