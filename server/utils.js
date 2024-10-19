460b99f1ad7a161a5d6719c069ca892fa093f0fe
Yosif Hamed <josif.hameed@gmail.com>
2024-10-19 12:33:45 +0300

    Stye elements

--- server/utils.js
+++ server/utils.js
@@ -9,13 +9,13 @@ dotenv.config()
 
 const parser = new Parser();
 const RSS_FEEDS = [
-    // 'https://feed.podbean.com/web3onfire/feed.xml',
+    'https://feed.podbean.com/web3onfire/feed.xml',
     'https://decrypt.co/feed',
-    // 'https://www.coindesk.com/arc/outboundfeeds/rss/?_gl=1*byjpx3*_up*MQ..*_ga*MTg1ODczMTQ3MC4xNzI5MjQ2MTc1*_ga_VM3STRYVN8*MTcyOTI0NjE3NC4xLjAuMTcyOTI0NjE3NC4wLjAuODQzMTk5OTI3',
+    'https://www.coindesk.com/arc/outboundfeeds/rss/?_gl=1*byjpx3*_up*MQ..*_ga*MTg1ODczMTQ3MC4xNzI5MjQ2MTc1*_ga_VM3STRYVN8*MTcyOTI0NjE3NC4xLjAuMTcyOTI0NjE3NC4wLjAuODQzMTk5OTI3',
     'https://cointelegraph.com/rss',
-    // 'https://bitcoinmagazine.com/.rss/full/',
-    // 'https://cryptoslate.com/feed/',
-    // 'https://www.vitoshacademy.com/feed/'
+    'https://bitcoinmagazine.com/.rss/full/',
+    'https://cryptoslate.com/feed/',
+    'https://www.vitoshacademy.com/feed/'
 ];
 
 
